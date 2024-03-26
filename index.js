2920da462e5984f740c395ccecc82669cedd75f9
Arto Ngguli Hunga <artorimex@gmail.com>
2024-03-26 21:03:14 +0800

    Selesai

---
+++ index.js
@@ -0,0 +1,46 @@
+// Perbedaan var,let dan const
+
+const number = 10;
+// number = 100;
+console.log(number);
+
+// Tiga car untuk deklarasi function
+// Fanction Arrow
+
+// Declaration
+function hello() {
+  console.log("Hellow");
+}
+
+// Expression
+const hellow = function () {
+  console.log("Hellow");
+};
+
+// Arrow
+const helloo = () => {
+  console.log("Hello");
+};
+
+// ES5
+function tambahh(a, b) {
+  return a + b;
+}
+
+// ES6
+const tambah = (a, b) => a + b;
+
+console.log(tambah(5, 9));
+
+// Contoh ES5
+let numbers = [1, 2, 3];
+angka = numbers;
+
+for (let i = 0; i < angka.length; i++) {
+  console.log(angka[i]);
+}
+
+// Contoh ES6
+numbers.forEach((Element) => {
+  console.log(Element);
+});
